eeca234007cdc8a565d1737468b126cfe4cdc3db
Daniil Sukharnikov <afterallspace@gmail.com>
2023-10-12 21:51:13 +0600

    kit: update tip component

--- app.vue
+++ app.vue
@@ -1,19 +1,11 @@
-<script setup lang="ts">
-const load = ref(false)
-
-const some = () => {
-  load.value = true
-  setTimeout(() => (load.value = false), 2000)
-}
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <UiSpace layout="container">
     <UiSpace layout="wrapper">
       <!-- Some -->
       <UiSpace display="row" pos="left">
-        <CustomButton title="" :loading="load" :icon="ICON_INFO" name="Генератор" @trigger="some" />
-        <CustomButton title="" mode="icon" :loading="load" :icon="ICON_INFO" @trigger="some" />
+        <CustomTip title="Error" message="Please doing something good..." />
       </UiSpace>
     </UiSpace>
   </UiSpace>
--- components/custom/tip.vue
+++ components/custom/tip.vue
@@ -1,26 +1,43 @@
 <template>
-  <UiSpace display="grid" block="def" class="tip">
+  <UiSpace display="grid" gap="sm" block="def" :class="{ tip: true, wave: wave }">
+    <UiIcon :name="ICON_INFO" size="ui" />
     <div class="mark"></div>
     <UiSpace display="col" gap="sm">
       <UiText type="h4" :text="title" />
       <UiText :text="message" />
     </UiSpace>
+    <TipWave v-if="wave" />
   </UiSpace>
 </template>
 
 <script setup lang="ts">
-defineProps<{ title: string; message: string }>()
+import TipWave from '@/assets/graphics/wave.vue'
+withDefaults(defineProps<{ title: string; message: string; wave: boolean }>(), { wave: true })
 </script>
 
 <style scoped lang="scss">
 .tip {
-  grid-template-columns: toRem(5) 1fr;
+  grid-template-columns: toRem(32) toRem(2) 1fr;
+}
+
+.wave {
+  position: relative;
+  z-index: 1;
+  overflow: hidden;
+  padding-bottom: var(--space-l);
+
+  svg {
+    position: absolute;
+    bottom: toRem(-2);
+    width: 100%;
+    z-index: -1;
+  }
 }
 
 .mark {
   width: 100%;
   height: 100%;
-  background-color: var(--m);
+  background-color: var(--txt-m);
   border-radius: var(--br-rad);
 }
 </style>
