abf6a1556e743f110a5bb2b40f6117acd0286856
Daniil Sukharnikov <afterallspace@gmail.com>
2023-12-18 14:17:17 +0600

    hotfix

--- components/u/badge.vue
+++ components/u/badge.vue
@@ -1,18 +1,27 @@
 <template>
   <USpace display="row" center gap="bit" class="badge">
-    <UIcon v-if="icon" size="sm" :name="icon" />
-    <div v-if="color" :style="`background-color: var(--${color})`" class="mark"></div>
+    <UIcon v-if="icon" size="sm" :name="icon" :style="color ? `color: var(--${color})` : ''" />
+    <div v-if="mode === 'mark'" :style="`background-color: var(--${color})`" class="mark"></div>
     <UText gray :text="text" />
   </USpace>
 </template>
 
 <script setup lang="ts">
-defineProps<{ text: string; icon?: string; color?: 'red' | 'yesllow' | 'green' }>()
+withDefaults(
+  defineProps<{
+    text: string
+    icon?: string
+    mode?: 'icon' | 'mark'
+    color?: 'red' | 'orange' | 'green'
+  }>(),
+  { mode: 'icon', icon: undefined, color: undefined }
+)
 </script>
 
 <style scoped lang="scss">
 .badge {
   border: toRem(1) solid var(--br);
+  background-color: var(--fg-m);
   border-radius: var(--br-rad);
   height: toRem(28);
   padding: toRem(5) var(--space-m);
